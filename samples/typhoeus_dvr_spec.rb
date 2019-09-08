ab8429e0b047435c6f2d3afc088ff92dd2dace7b
jimmy <jimmy@jimmysun-mbr.local>
2019-09-07 21:18:51 -0700

    test better

--- samples/typhoeus_dvr_spec.rb
+++ samples/typhoeus_dvr_spec.rb
@@ -11,6 +11,17 @@ def remove_previous_record(request)
   File.exist?(filename).must_equal false
 end
 
+def verify_response(response, url)
+  response.wont_be_nil
+  response.effective_url.must_equal url
+  nil
+end
+
+def verify_recorded_file(filename, url)
+  File.exist?(filename).must_equal true
+  File.read(filename).include?(url).must_equal true
+end
+
 class TyphoeusDvrSpec < Minitest::Test
   describe "TyphoeusDvrSpec" do
     before do
@@ -27,42 +38,47 @@ class TyphoeusDvrSpec < Minitest::Test
     describe "RecordMode" do
       it "Should write response to file too" do
         Typhoeus.record_mode = Typhoeus::RECORD_MODE_RECORD
+        @request.on_complete.clear
         @request.on_complete do |response|
-          response.effective_url.must_equal @url
+          verify_response(response, @url)
+          verify_recorded_file(@filename, @url)
         end
         @request.run
-        File.exist?(@filename).must_equal true
-        File.read(@filename).include?(@url).must_equal true
 
         # Do it again using Hydra
         remove_previous_record @request
         hydra = Typhoeus::Hydra.new
         hydra.queue @request
         hydra.run
-        File.exist?(@filename).must_equal true
-        File.read(@filename).include?(@url).must_equal true
+        verify_recorded_file(@filename, @url)
       end
     end
 
     describe "ReplayMode" do
       it "Should replay response from file" do
+        # First create a new record file
         @request.on_complete.clear
         Typhoeus.record_mode = Typhoeus::RECORD_MODE_RECORD
+        @request.on_complete.clear
+        @request.on_complete do |response|
+          verify_response(response, @url)
+          verify_recorded_file(@filename, @url)
+        end
         @request.run
-        File.exist?(@filename).must_equal true
-        File.read(@filename).include?(@url).must_equal true
 
-        # record file is not calibrated to snafu
+        # Calibrate record file to snafu url
         blob = File.read(@filename)
         blob = blob.gsub(/fubar/, 'snafu')
         File.write(@filename, blob)
+        calibrated_url = 'http://snafu/'
 
         # Now in replay mode
         Typhoeus.record_mode = Typhoeus::RECORD_MODE_REPLAY
-        @request.on_complete do |response|
-          response.effective_url.must_equal 'http://snafu/'
-        end
         @request.run
+        @request.on_complete.clear
+        @request.on_complete do |response|
+          verify_response(response, calibrated_url)
+        end
 
         # Do it again using Hydra
         hydra = Typhoeus::Hydra.new
